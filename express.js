4c1f1f90ff05d1895a4bac32116009547613f498
Mateus Böhr <153296791+Mateus1502@users.noreply.github.com>
2024-11-06 20:02:08 -0300

    Update express.js

--- express.js
+++ express.js
@@ -4,22 +4,52 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-const db = new sqlite3.Database('./itemsdb.sqlite',(err) =>{
+
+const db = new sqlite3.Database('./itemsdb.sqlite', (err) => {
     if(err) {
-        console.err('Avisei que ia dar merda');
+        console.err('Deu erro!');
     } else {
-        console.log('Deu certo');
+        console.log('Deu certo!');
     }
 });
 
-db.run(`CREATE TABLE IF NOT EXISTS items(
+db.run(`CREATE TABLE IF NOT EXISTS items (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL 
-    descricao TEXT 
-    dataCriacao TEXT DEFAULT CURRENT_TIMESTAMP) `), (err) => {
-        if (err){
-            console.err('Deu merda bro');
-        } else {
-            console.log('Deu tudo certo')
+    name TEXT NOT NULL,
+    descricao TEXT,
+    dataCriacao TEXT DEFAULT CURRENT_TIMESTAMP)`, (err) => {
+        if (err) {
+            console.error('Deu erro ao criar a tabela');
         }
-    }
+});
+
+app.post("/items", (req,res)=>{
+    const  {name , descricao } = req.body;
+    const query = `INSERT INTO items(name, descricao) VALUES (?,?)`// ?? para impedir ataques maliciosos
+
+    db.all(query, [name, descricao], (err) => {
+        if (err){
+            res.status(400).json({message : err.message});
+
+        }else {
+            res.status(201).json({id: this.lastID , name , descricao});
+        }
+
+    })
+    
+});
+
+app.get('/items', (req,res) => {
+    const query = "SELECT * FROM  items";
+    db.run(query,[],(err,rows)=>{
+        if(err){
+            console.error({message:err.message});
+        }else{
+            res.status(200).json(rows);
+        }
+    })
+
+});
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
